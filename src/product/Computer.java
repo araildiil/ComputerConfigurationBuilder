5807ab8cd739dc898d2f21f2546fb48275b66c1a
ademaaitu <ademaamangeldi111@gmail.com>
2026-09-13 02:54:45 +0500

    Add Computer product class

---
+++ src/product/Computer.java
@@ -0,0 +1,30 @@
+package product;
+
+public class Computer {
+    private final String cpu;
+    private final int ramGb;
+    private final String gpu;
+    private final int storageGb;
+    private final boolean hasWifi;
+    private final boolean hasBluetooth;
+    private final int warrantyYears;
+
+    private Computer(String cpu, int ramGb, String gpu, int storageGb,
+                     boolean hasWifi, boolean hasBluetooth, int warrantyYears) {
+        this.cpu = cpu;
+        this.ramGb = ramGb;
+        this.gpu = gpu;
+        this.storageGb = storageGb;
+        this.hasWifi = hasWifi;
+        this.hasBluetooth = hasBluetooth;
+        this.warrantyYears = warrantyYears;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{cpu='" + cpu + "', ramGb=" + ramGb +
+                ", gpu='" + gpu + "', storageGb=" + storageGb +
+                ", hasWifi=" + hasWifi + ", hasBluetooth=" + hasBluetooth +
+                ", warrantyYears=" + warrantyYears + '}';
+    }
+}
